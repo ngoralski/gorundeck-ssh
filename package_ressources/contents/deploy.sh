2d6ec90ed204c34cf3fa409b6fc61ae5ad0519c1
Nicolas GORALSKI <nicolas@goralski.fr>
2022-12-21 18:43:06 +0100

    Remove bad uploaded file. Add the wrapper for go binary.

--- package_ressources/contents/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#go build cmd/gorundeck-ssh/main.go
-
-scp contents/build_linux root@rundeck:/var/lib/rundeck/libext/cache/gorundeck-ssh-1.0-plugin/go_build_main_go_linux
---
+++ package_ressources/contents/wrapper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+plugin_dir=${BASH_SOURCE[0]%/*}
+
+binary=${plugin_dir}/gorundeck-ssh
+
+${binary}
